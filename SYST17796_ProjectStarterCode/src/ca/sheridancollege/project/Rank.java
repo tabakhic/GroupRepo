b9fc2147e798345626f56136d6b79c1906529989
tabakhic <cyrust2004@gmail.com>
2023-02-14 21:29:25 -0500

    New Commit (Updated Code)

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Rank.java
+++
@@ -1,9 +0,0 @@
-package ca.sheridancollege.project;
-
-/**
- * Author: Cyrus Tabakhi
- * An enum representing the thirteen possible ranks of a standard deck of cards.
- */
-public enum Rank {
-    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
-}
--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Suit.java
+++
@@ -1,9 +0,0 @@
-package ca.sheridancollege.project;
-
-/**
- * Author: Cyrus Tabakhi
- * An enum representing the four suits of a standard deck of cards.
- */
-public enum Suit {
-    HEARTS, DIAMONDS, CLUBS, SPADES
-}
